61fa12b4c089743acce4117e3c8211eac9189224
Squid4572 <80015221+Squid4572@users.noreply.github.com>
2022-01-28 15:47:20 -0500

    Initial Commit

---
+++ JustText Mini/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  UI Test
+//
+//  Created by Dante Vaughn on 12/19/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet var window: NSWindow!
+}
